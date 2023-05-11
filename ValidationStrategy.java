3c1465b7a1e0cc1e04db7ad59f4526827f800433
Roaa Talat <100158472+roaa0@users.noreply.github.com>
2023-05-11 23:59:51 +0300

    Add files via upload

---
+++ ValidationStrategy.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.toffee.EnterUser;
+
+/**
+ *
+ * @author Salma Mamdoh
+ * This an interface of validation Strategy
+ * for applying strategy pattern
+ * 
+ */
+public interface ValidationStrategy {
+     boolean validate(String input);
+}
